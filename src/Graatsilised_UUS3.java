1998cdd9a6bfdadd344194c5187e4ba21efcef9a
Miron <miron.storozhev96@gmail.com>
2018-03-26 22:26:36 +0300

    Remove useless imports

--- src/Graatsilised_UUS3.java
+++ src/Graatsilised_UUS3.java
@@ -1,16 +1,20 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 
 
 public class Graatsilised_UUS3 {
-    private static final short n = 6;//servade arv, tippe 1 võrra rohkem !!!!!
-    private static final short rek = 2; //rek - rekursiooni tase, millest tuleks jagada funktsioon genereeriGraaf lõimedeks
-    private static final short arvutiJagamine = -1;
+    private static final short n = 7;//servade arv, tippe 1 võrra rohkem !!!!!
+    private static final short rek = 4; //rek - rekursiooni tase, millest tuleks jagada funktsioon genereeriGraaf lõimedeks
     private static short arvutiNR;
     private static AtomicInteger counter = new AtomicInteger(0);
     private static volatile Set<GraatsilineGraaf> unikaalsed;
+    private static final short[][] options = {{0,0,0,0},{0,0,0,1},{0,0,0,2},{0,0,0,3},{0,0,1,0},{0,0,1,1},{0,0,1,2},{0,0,1,3},{0,0,2,0},{0,0,2,1},{0,0,2,2},{0,0,2,3},{0,1,0,0},{0,1,0,1},{0,1,0,2},{0,1,0,3},{0,1,1,0},{0,1,1,1},{0,1,1,2},{0,1,1,3},{0,1,2,0},{0,1,2,1},{0,1,2,2},{0,1,2,3}};
 
     //Lõim töötab eraldi meetodis
     private static Set<GraatsilineGraaf> genereeriGraaf(int pikkus, short[] servad, Set<GraatsilineGraaf> threadiGraafid) {
@@ -45,22 +49,16 @@ public class Graatsilised_UUS3 {
             }
         }
         //iga arvuti saab arvutiNR, mis määrab igale arvutile tema vastava haru puus.
-        else if (pikkus == n - arvutiJagamine) {
+        else {
             short[] uus = new short[servad.length + 1];
-            uus[0] = (short)(arvutiNR % (arvutiJagamine+1)); //iga arvuti saab endale ühe kindla haru. Kasutan arvutiJagamine + 1 jagamisega jääki.
+            uus[0] = options[arvutiNR][n-pikkus];
+            System.out.println(uus[0]);
+            //uus[0] = (short)(arvutiNR % (arvutiJagamine+1)); //iga arvuti saab endale ühe kindla haru. Kasutan arvutiJagamine + 1 jagamisega jääki.
             // see tähendab, et tasemel 3 jagatakse 24 arvutiks
             // Tekivad arvud 1,2,3,...,0,1,2,3,...,0 jne.
             // ülejäänud kopeerime
             System.arraycopy(servad, 0, uus, 1, servad.length);
             genereeriGraaf((short) (pikkus - 1), uus);
-        }
-        else {
-            for (short i = 0; i <= n - pikkus; i++) {
-                short[] uus = new short[servad.length + 1];
-                uus[0] = i; //fikseerime serva pikkusega "pikkus"
-                System.arraycopy(servad, 0, uus, 1, servad.length);
-                genereeriGraaf((pikkus - 1), uus);
-            }
         }
     }
 
@@ -80,24 +78,31 @@ public class Graatsilised_UUS3 {
         System.out.println("Aega kulus " + (stop - start) / 1000.0 + " sekundit");
         System.out.println("TOTAL:" + counter.toString());
 
-
+        /*
         for (GraatsilineGraaf graaf : unikaalsed) {
             System.out.println(graaf);
         }
+        */
 
 
 
-        /*
+
         try {
             PrintWriter valjund = new PrintWriter(new FileWriter("isomorfsus.txt"), true);
+            PrintWriter valjund1 = new PrintWriter(new FileWriter("isomorfsus_1.txt"), true);
+            PrintWriter valjund2 = new PrintWriter(new FileWriter("isomorfsus_2.txt"), true);
+            PrintWriter valjund3 = new PrintWriter(new FileWriter("isomorfsus_3.txt"), true);
             for (GraatsilineGraaf graaf : unikaalsed) {
                 valjund.println(graaf);
+                valjund1.println(graaf);
+                valjund2.println(graaf);
+                valjund3.println(graaf);
             }
             valjund.close();
         } catch (IOException e) {
             System.out.println("S/V viga: " + e);
         }
-        */
+
     }
 }
 
